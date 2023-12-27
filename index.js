7d50fb8f16eccf5eecb74f1e20b9b5e0ec87d878
Shubham Shukla <shukla111shubh@gmail.com>
2023-12-27 16:36:14 +0530

    added database configuration and utilities

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import 'dotenv/config'
-import express from "express";
+// import 'dotenv/config'
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-})
+// app.listen(process.env.PORT, () => {
+//   console.log(`Example app listening on port ${process.env.PORT}`);
+// })
